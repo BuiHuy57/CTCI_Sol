860dd6fb5e1e3e5ad8d0b276614729c9bf837d55
Ryan Bui <buihuyryan@gmail.com>
2022-01-30 15:57:42 -0500

    Move chapter 2 tests to separate file

---
+++ Chapter2/Kth_To_Last.py
@@ -0,0 +1,15 @@
+""" Implement an algorithm to find the kth to last element of a singly linked list """
+from data_structs import (Node, SLinkedList)
+class Solution():
+    def kth_to_last(self, linkedlist:SLinkedList = None, k:int = None) -> Node:
+        """Function that returns kth to last element of singly linked list
+
+        Args:
+            linkedlist (SLinkedList, optional): Singly linked list argument. Defaults to None.
+            k (int, optional): Element to find from end of list. Defaults to None.
+
+        Returns:
+            Node: kth last element to be returned
+        """
+        
+        return None
---
+++ test_ch2.py
@@ -0,0 +1,40 @@
+import unittest
+
+from data_structs import (Node, SLinkedList)
+from Chapter2 import (Remove_Dups, Kth_To_Last)
+
+class Test_Remove_Dups(unittest.TestCase):
+    def setUp(self):
+        self.solution = Remove_Dups.Solution()
+        
+    def test_remove_dups(self):
+        linkedlist = SLinkedList([9, 8, 7, 7])
+        
+        actual = self.solution.remove_dups(linkedlist)
+        expected = SLinkedList([9, 8, 7])
+        
+        self.assertEqual(actual, expected)
+        
+    def test_all_dups(self):
+        linkedlist = SLinkedList([7,7,7,7])
+        
+        actual = self.solution.remove_dups(linkedlist)
+        expected = SLinkedList([7])
+        
+        self.assertEqual(actual, expected)
+     
+class Test_Kth_Last_El(unittest.TestCase):
+    def setUp(self):
+        self.solution = Kth_To_Last.Solution()
+        
+    def test_kth_last_el(self):
+        linkedlist = SLinkedList([9, 8, 7, 7])
+        k = 3
+        
+        actual = self.solution.kth_to_last(linkedlist, k)
+        expected = ""
+        
+        self.assertEqual("", expected)
+    
+if __name__ == "__main__":
+    unittest.main()
