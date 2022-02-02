bd27812fe7cd567093fcf5a17aeb9edc68f86440
Ryan Bui <buihuyryan@gmail.com>
2022-02-01 19:12:41 -0500

    Solution for Remove Dups

--- Chapter2/Remove_Dups.py
+++ Chapter2/Remove_Dups.py
@@ -1,6 +1,6 @@
 """ Write code to remove duplicates from an unsorted linked list.
     How would you solve this problem if a temporary buffer is not allowed? """
-from data_structs import (SLinkedList)
+from data_structs import (Node, SLinkedList)
 
 
 class Solution:
@@ -21,36 +21,18 @@ class Solution:
         item_dict = {}
 
         ptr = linkedlist.head
+        prev_ptr: Node = None
+        size = linkedlist.getsize()
 
         while ptr is not None:
             if ptr.data not in item_dict:
                 item_dict[ptr.data] = 1
+                prev_ptr = ptr
             else:
-                item_dict[ptr.data] += 1
+                prev_ptr.next = ptr.next
+                size -= 1
 
             ptr = ptr.next
-
-        size = linkedlist.getsize()
-        print(size)
-        for key in item_dict.keys():
-            if item_dict[key] > 1:
-                curr_ptr = linkedlist.head
-                prev_ptr = linkedlist.head
-
-                while curr_ptr is not None and item_dict[key] > 1:
-                    if curr_ptr.data == key:
-                        print("HERE")
-                        prev_ptr.next = curr_ptr.next
-                        tmp_ptr = curr_ptr
-                        curr_ptr = curr_ptr.next
-                        tmp_ptr.next = None
-                        size -= 1
-                        item_dict[key] -= 1
-                    else:
-                        prev_ptr = curr_ptr
-                        curr_ptr = curr_ptr.next
-
-        print(f"SIZE: {size}")
 
         linkedlist.setsize(size)
 
