133f0c7823b22f22e1c8e03f309c90a78abba620
Ryan Bui <buihuyryan@gmail.com>
2022-02-08 19:50:15 -0500

    Animal Shelter Solution

--- Chapter3/Animal_Shelter.py
+++ Chapter3/Animal_Shelter.py
@@ -2,22 +2,67 @@
     People must adopt either the "oldest" (based on arrival time) of all animals at the shelter, or they can select whether they would prefer a dog or a cat (and will receive the oldest animal of that type).
     They cannot select which specific animal they would like. Create the data structures to maintain this system and implement operations such as enqueue, dequeueAny, dequeueDog, and dequeueCat.
     You may use the built-in LinkedList data structure """
-    
-from data_structs import Node
+
+from data_structs import Node, SLinkedList
 
 
 class Animal_Shelter():
-    def __init__(self):
-        return None
-    
-    def enqueue(self, name: str, type: str):
-        return None
-    
-    def dequeueAny(self):
-        return None
-    
-    def dequeueDog(self):
-        return None
-    
-    def dequeueCat(self):
-        return None
+    def __init__(self) -> None:
+        self.shelter = SLinkedList([])
+        self.dogs = SLinkedList([])
+        self.cats = SLinkedList([])
+
+    def enqueue(self, name: str = "", type: str = "") -> None:
+        self.shelter.insert(name)
+        if type.lower() == "dog":
+            self.dogs.insert(name)
+        elif type.lower() == "cat":
+            self.cats.insert(name)
+
+    def dequeueAny(self) -> Node:
+        if self.shelter.head is None:
+            raise Exception("Can't dequeue empty stack")
+
+        animal = self.shelter.pop()
+        if animal.data == self.dogs.peek().data:
+            self.dogs.pop()
+        elif animal.data == self.cats.peek().data:
+            self.cats.pop()
+
+        return animal
+
+    def dequeueDog(self) -> Node:
+        if self.dogs.head is None:
+            raise Exception("Can't dequeue empty stack")
+
+        animal = self.dogs.pop()
+        ptr = self.shelter.head
+        prev_ptr: Node = None
+
+        while ptr is not None:
+            if ptr.data == animal.data:
+                prev_ptr.next = ptr.next
+                ptr = prev_ptr
+            else:
+                prev_ptr = ptr
+                ptr = ptr.next
+
+        return animal
+
+    def dequeueCat(self) -> Node:
+        if self.cats.head is None:
+            raise Exception("Can't dequeue empty stack")
+
+        animal = self.cats.pop()
+        ptr = self.shelter.head
+        prev_ptr: Node = None
+
+        while ptr is not None:
+            if ptr.data == animal.data:
+                prev_ptr.next = ptr.next
+                ptr = prev_ptr
+            else:
+                prev_ptr = ptr
+                ptr = ptr.next
+
+        return animal
--- test_ch3.py
+++ test_ch3.py
@@ -12,19 +12,20 @@ class Test_Stack_Min(unittest.TestCase):
         self.stack_min.push(7)
         self.stack_min.push(8)
         self.stack_min.pop()
-        
+
         actual = self.stack_min.min()
         expected = Node(7)
-        
+
         self.assertEqual(actual, expected)
-    
+
     def test_empty_pop(self):
         with self.assertRaises(Exception):
             self.stack_min.pop()
-    
+
     def test_empty_min(self):
         with self.assertRaises(Exception):
             self.stack_min.min()
+
 
 class Test_Animal_Shelter(unittest.TestCase):
     def setUp(self):
@@ -33,20 +34,43 @@ class Test_Animal_Shelter(unittest.TestCase):
     def test_funcs(self):
         self.shelter.enqueue("Marvin", "Dog")
         self.shelter.enqueue("Sparkey", "Cat")
-        
+        self.shelter.enqueue("Bob", "Dog")
+        self.shelter.enqueue("Bill", "Cat")
+        self.shelter.enqueue("Jake", "Cat")
+
         actual = self.shelter.dequeueAny()
-        expected = Node("Sparkey")
-        
+        expected = Node("Jake")
+
         self.assertEqual(actual, expected)
-    
+
+    def test_cats(self):
+        print("test_cats")
+        self.shelter.enqueue("Jake", "Cat")
+        self.shelter.enqueue("Marvin", "Dog")
+
+        actual = self.shelter.dequeueCat()
+        expected = Node("Jake")
+
+        self.assertEqual(actual, expected)
+
+    def test_dogs(self):
+        print("test_dogs")
+        self.shelter.enqueue("Marvin", "Dog")
+        self.shelter.enqueue("Jake", "Cat")
+
+        actual = self.shelter.dequeueDog()
+        expected = Node("Marvin")
+
+        self.assertEqual(actual, expected)
+
     def test_empty_pop(self):
         with self.assertRaises(Exception):
             self.shelter.dequeueAny()
-    
+
     def test_empty_cats(self):
         with self.assertRaises(Exception):
             self.shelter.dequeueCat()
-    
+
     def test_empty_dogs(self):
         with self.assertRaises(Exception):
             self.shelter.dequeueDog()
